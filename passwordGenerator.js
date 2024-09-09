7da559a30de154e0914a99da7a07a368e3a3f382
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-09-09 13:26:49 +0100

    CSS - Initial styling

---
+++ passwordGenerator.js
@@ -0,0 +1,24 @@
+// //////////////////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////////////////
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ CUSTOM SLIDER ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+const slider = document.querySelector('#charLengthSlider');
+const sliderValue = document.querySelector('#charLength');
+const rootStyles = getComputedStyle(document.documentElement);
+const sliderEmtpyClr = rootStyles.getPropertyValue('--clr-bg-body').trim();
+const sliderValueClr = rootStyles.getPropertyValue('--clr-green').trim();
+
+// SET INITIAL SLIDER VALUE COUNT
+sliderValue.textContent = slider.value;
+
+// WHEN SLIDER VALUE CHANGES, UPDATE COUNT
+slider.addEventListener('input', (event) => {
+  const sliderPosition = event.target.value;
+
+  sliderValue.textContent = sliderPosition;
+
+  const progress = (sliderPosition / slider.max) * 100;
+
+  slider.style.background = `linear-gradient(to right, ${sliderValueClr} ${progress}%, ${sliderEmtpyClr} ${progress}%)`;
+});
