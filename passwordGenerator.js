572ef4d2472a4c03ce59cb2ca8dc51b88e0edef9
Jot Hogben <jot_hogben@hotmail.co.uk>
2024-09-11 16:13:02 +0100

    Javascript - hardcoding custom slider colors

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -203,26 +203,20 @@ const collectInitialData = () => {
 
 const slider = document.querySelector('#charLengthSlider');
 const sliderValue = document.querySelector('#charLength');
-// const rootStyles = getComputedStyle(document.documentElement);
-// const sliderEmtpyClr = rootStyles.getPropertyValue('--clr-bg-body').trim();
-// const sliderValueClr = rootStyles.getPropertyValue('--clr-green').trim();
+const sliderProgress = '#a4ffaf';
+const sliderEmpty = '#08070b';
 
 // SET INITIAL SLIDER VALUE COUNT
 sliderValue.textContent = slider.value;
 
 // WHEN SLIDER VALUE CHANGES, UPDATE COUNT
-['input', 'touchmove', 'wheel'].forEach((inputMode) => {
-  slider.addEventListener(inputMode, (event) => {
+  slider.addEventListener('input', (event) => {
     const sliderPosition = event.target.value;
-
-    sliderValue.textContent = sliderPosition;
-
     const progress = (sliderPosition / slider.max) * 100;
 
-    // slider.style.background = `linear-gradient(to right, ${sliderValueClr} ${progress}%, ${sliderEmtpyClr} ${progress}%)`;
-    slider.style.background = `linear-gradient(to right, #a4ffaf, #a4ffaf ${progress}%, #08070b ${progress}%)`;
+    sliderValue.textContent = sliderPosition;
+    slider.style.background = `linear-gradient(to right, ${sliderProgress}, ${sliderProgress} ${progress}%, ${sliderEmpty} ${progress}%)`;
   });
-});
 
 // //////////////////////////////////////////////////////////////////////////////////////
 // //////////////////////////////////////////////////////////////////////////////////////
